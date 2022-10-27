3e92b3a6fe5e7c97a412352d1877e597ba81d506
Marc Müller <marc.mueller04@gmail.com>
2022-10-27 19:34:23 +0200

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -23,7 +23,11 @@ class AuthModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     username: str
     password: str
-    email: EmailStr
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
 
 
 class DataModel(BaseModel):
