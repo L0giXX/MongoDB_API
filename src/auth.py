97ed0468af2a135e0a1d371f7d8303fc59ed87c3
Marc Müller <marc.mueller04@gmail.com>
2022-10-27 19:34:18 +0200

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,2 +1,12 @@
 import jwt
-from .schemas import AuthModel
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+
+
+class Authhandler():
+    security = HTTPBearer()
+    ctx = CryptContext(schemes=["sha256_crypt"])
+
+    def get_password_hash(self, password):
+        return self.ctx.hash(password)
