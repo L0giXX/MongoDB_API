1404fe5f53fcb2597d3851d7f1c2073af6590ef1
Marc Müller <marc.mueller04@gmail.com>
2022-11-14 12:32:25 +0100

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -5,7 +5,7 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 
-class Authhandler():
+class AuthHandler():
     security = HTTPBearer()
     ctx = CryptContext(schemes=["sha256_crypt"])
     secret_key = "4aa771add5cf42f9ade97b260699172d6eb12efe9327a45621c0ac92c753c5d0"
@@ -16,15 +16,20 @@ class Authhandler():
     def verify_password(self, pwd, hashed_pwd):
         return self.ctx.verify(pwd, hashed_pwd)
 
-    def encode_token(self, user):
+    def encode_token(self, user, type):
         payload = {
-            # expiration time
-            "exp": datetime.utcnow() + timedelta(days=0, minutes=5),
             # issued at
             "iat": datetime.utcnow(),
             # subject
             "sub": user
         }
+        # Expiration time
+        if type == "access":
+            payload.update({"exp": datetime.utcnow()+timedelta(minutes=5)})
+        elif type == "refresh":
+            payload.update({"exp": datetime.utcnow()+timedelta(hours=2)})
+        else:
+            raise HTTPException(status_code=401, detail="Invalid type")
         return jwt.encode(payload, self.secret_key, algorithm="HS256")
 
     def decode_token(self, token):
