9329a1068521e5fdb3d138e2d45aa080ed980207
Marc Müller <marc.mueller04@gmail.com>
2022-11-14 12:31:36 +0100

    Create auth.py

---
+++ docker/app/auth.py
@@ -0,0 +1,45 @@
+import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+
+class AuthHandler():
+    security = HTTPBearer()
+    ctx = CryptContext(schemes=["sha256_crypt"])
+    secret_key = "4aa771add5cf42f9ade97b260699172d6eb12efe9327a45621c0ac92c753c5d0"
+
+    def get_password_hash(self, pwd):
+        return self.ctx.hash(pwd)
+
+    def verify_password(self, pwd, hashed_pwd):
+        return self.ctx.verify(pwd, hashed_pwd)
+
+    def encode_token(self, user, type):
+        payload = {
+            # issued at
+            "iat": datetime.utcnow(),
+            # subject
+            "sub": user
+        }
+        # Expiration time
+        if type == "access":
+            payload.update({"exp": datetime.utcnow()+timedelta(minutes=5)})
+        elif type == "refresh":
+            payload.update({"exp": datetime.utcnow()+timedelta(hours=2)})
+        else:
+            raise HTTPException(status_code=401, detail="Invalid type")
+        return jwt.encode(payload, self.secret_key, algorithm="HS256")
+
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms="HS256")
+            return payload["sub"]
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail="Signature expired")
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
+        return self.decode_token(auth.credentials)
