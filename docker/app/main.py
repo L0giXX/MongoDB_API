5de84abe7b9f81360f6aa21ac8485b6eb97d0c17
Marc Müller <marc.mueller04@gmail.com>
2022-11-22 08:22:13 +0100

    Update main.py

--- docker/app/main.py
+++ docker/app/main.py
@@ -2,11 +2,13 @@ import pymongo
 from fastapi import FastAPI, status, Depends
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from .auth import AuthHandler
 from .models import AuthModel, DataModel
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
+
 
 client = pymongo.MongoClient(config["MONGODB_URL"])
 db = client["ESP32DB"]
@@ -14,7 +16,14 @@ dataC = db["data"]
 profileC = db["profile"]
 
 app = FastAPI()
-auth_handler = AuthHandler()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
+Auth_Handler = AuthHandler()
 
 
 class DataHandler():
@@ -51,10 +60,8 @@ class DataHandler():
 @app.post("/register")
 def register(req: AuthModel):
     req = jsonable_encoder(req)
-    if profileC.find_one({"username": req["username"]}):
-        return Response(status_code=status.HTTP_400_BAD_REQUEST, content="Username bereits benutzt: [" + req["username"]+"]")
-    hashed_password = auth_handler.get_password_hash(req["password"])
-    req["password"] = hashed_password
+    req["password"] = Auth_Handler.authenticate_user(
+        profileC, req["username"], req["password"])
     newData = profileC.insert_one(req)
     curData = profileC.find_one({"_id": newData.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=curData)
@@ -63,21 +70,14 @@ def register(req: AuthModel):
 @app.post("/login")
 def login(req: AuthModel):
     tmp = []
-    user: str
     token: str
     req = jsonable_encoder(req)
-    # Authenticate user and verify password
-    if profileC.find_one({"username": req["username"]}):
-        user = req["username"]
-        for x in profileC.find({"username": user}):
-            tmp.append(x)
-        if auth_handler.verify_password(req["password"], tmp[0]["password"]):
-            token = auth_handler.encode_token(user, "access")
-            return Response(status_code=status.HTTP_201_CREATED, content="Token: "+token)
-        else:
-            return Response(status_code=status.HTTP_401_UNAUTHORIZED, content="Passwort nicht korrekt!")
+    tmp = Auth_Handler.get_user(profileC, req["username"])
+    if Auth_Handler.verify_password(req["password"], tmp[0]["password"]):
+        token = Auth_Handler.encode_token(tmp[0]["username"])
+        return Response(status_code=status.HTTP_201_CREATED, content="Token: "+token)
     else:
-        return Response(status_code=status.HTTP_401_UNAUTHORIZED, content="Username nicht registriert!")
+        return Response(status_code=status.HTTP_401_UNAUTHORIZED, content="Passwort nicht korrekt!")
 
 
 @app.get('/unprotected')
@@ -86,12 +86,12 @@ def unprotected():
 
 
 @app.get("/protected")
-def protected(username=Depends(auth_handler.auth_wrapper)):
-    return Response(status_code=status.HTTP_200_OK, content="Name: "+username)
+def protected(username=Depends(Auth_Handler.auth_wrapper)):
+    return Response(status_code=status.HTTP_200_OK, content="Benutzer: "+username)
 
 
 @app.get("/profile/get")
-def getProfile():
+def getProfile(request=Depends(Auth_Handler.auth_wrapper)):
     tmp = []
     for x in profileC.find():
         tmp.append(x)
@@ -102,28 +102,32 @@ def getProfile():
 def addData(req: DataModel):
     req = jsonable_encoder(req)
     if req["sensor"] == "BME680":
-        req.pop("power")    # delete key: power
+        req["temp"] = round(req["temp"], 2)
+        req["humi"] = round(req["humi"], 2)
+        req["press"] = round(req["press"], 2)
+        req.pop("power")
         if req["temp"] < -20 or req["temp"] > 50:
             return Response(content="Keine normalen Temperaturen gemessen: [" + str(req["temp"])+"]")
 
-        elif req["humi"] < 40 or req["humi"] > 70:
+        elif req["humi"] < 20 or req["humi"] > 70:
             return Response(content="Keine normale Luftfeuchtigkeit gemessen: [" + str(req["humi"])+"]")
 
-        elif req["press"] < 900 or req["press"] > 1100:
+        elif req["press"] < 0 or req["press"] > 1.5:
             return Response(content="Keinen normalen Druck gemessen: [" + str(req["press"])+"]")
 
         else:
             newData = dataC.insert_one(req)
             curData = dataC.find_one({"_id": newData.inserted_id})
-            return JSONResponse(status_code=status.HTTP_201_CREATED, content=curData)
+            return JSONResponse(status_code=201, content=curData)
 
     elif req["sensor"] == "CT-Sensor":
-        req.pop("temp")     # delete key: temp
-        req.pop("humi")     # delete key: humi
-        req.pop("press")    # delete key: press
+        req["power"] = round(req["power"], 2)
+        req.pop("temp")
+        req.pop("humi")
+        req.pop("press")
         newData = dataC.insert_one(req)
         curData = dataC.find_one({"_id": newData.inserted_id})
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content=curData)
+        return JSONResponse(status_code=201, content=curData)
     else:
         return Response(content="Falscher Sensor! ["+req["sensor"])
 
@@ -141,10 +145,18 @@ def getallData():
     return JSONResponse(status_code=status.HTTP_200_OK, content=tmp)
 
 
-@app.get("/data/get/latest")
+@app.get("/data/get/latest/air")
 def getlatestData():
     tmp = []
-    for x in dataC.find():
+    for x in dataC.find({"sensor": "BME680"}):
+        tmp.append(x)
+    return JSONResponse(status_code=status.HTTP_200_OK, content=tmp[-1])
+
+
+@app.get("/data/get/latest/power")
+def getlatestData():
+    tmp = []
+    for x in dataC.find({"sensor": "CT-Sensor"}):
         tmp.append(x)
     return JSONResponse(status_code=status.HTTP_200_OK, content=tmp[-1])
 
