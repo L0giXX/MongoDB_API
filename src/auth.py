4a9eaa582c4fa5185a957ddd2beb00a4769177d1
Marc Müller <marc.mueller04@gmail.com>
2022-12-04 23:21:23 +0100

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,61 +1,92 @@
-import jwt
-from fastapi import HTTPException, Depends
+from jose import JWTError, jwt
+from fastapi import HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from dotenv import dotenv_values
+from .models import AuthModel
 
 config = dotenv_values("src/.env")
 
 
-class AuthHandler():
-    oauth2 = OAuth2PasswordBearer(tokenUrl="login")
-    ctx = CryptContext(schemes=["sha256_crypt"])
-    secret_key = config["SECRET_KEY"]
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+ctx = CryptContext(schemes=["sha256_crypt"])
+secret_key = config["SECRET_KEY"]
+ALGORITHM = "HS256"
 
-    def get_password_hash(self, pwd):
-        return self.ctx.hash(pwd)
 
-    def verify_password(self, pwd, hashed_pwd):
-        return self.ctx.verify(pwd, hashed_pwd)
+def verify_password(plain_password, hashed_password):
+    return ctx.verify(plain_password, hashed_password)
 
-    def authenticate_user(self, db, user, pwd):
-        if db.find_one({"username": user}):
-            raise HTTPException(
-                status_code=400, detail="Username bereits vergeben")
+
+def get_password_hash(password):
+    return ctx.hash(password)
+
+
+def register_user(db, user, pwd):
+    if db.find_one({"username": user}):
+        raise HTTPException(
+            status_code=400, detail="Username bereits vergeben")
+    else:
+        hashed_pwd = get_password_hash(pwd)
+        return hashed_pwd
+
+
+def get_user(db, username):
+    tmp = []
+    if db.find_one({"username": username}):
+        for x in db.find({"username": username}):
+            tmp.append(x)
+        return tmp[0]["username"]
+    else:
+        return None
+
+
+def authenticate_user(db, username, password):
+    tmp = []
+    if db.find_one({"username": username}):
+        for x in db.find({"username": username}):
+            tmp.append(x)
+    else:
+        return False
+    if not tmp[0]["password"]:
+        return False
+    if not verify_password(password, tmp[0]["password"]):
+        return False
+    else:
+        return tmp[0]["username"]
+
+
+def create_access_token(username):
+    payload = {
+        # issued at
+        "iat": datetime.utcnow(),
+        # expiration time
+        "exp": datetime.utcnow() + timedelta(minutes=120),
+        # subject
+        "sub": username
+    }
+    encoded_jwt = jwt.encode(
+        payload, secret_key, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
         else:
-            hashed_pwd = self.get_password_hash(pwd)
-            return hashed_pwd
+            return username
+    except JWTError:
+        raise credentials_exception
 
-    def get_user(self, db, user):
-        tmp = []
-        if db.find_one({"username": user}):
-            for x in db.find({"username": user}):
-                tmp.append(x)
-            return tmp
-        else:
-            raise HTTPException(
-                status_code=400, detail="Username nicht vorhanden")
 
-    def encode_token(self, user):
-        payload = {
-            # issued at
-            "iat": datetime.utcnow(),
-            # expiration time
-            "exp": datetime.utcnow() + timedelta(minutes=120),
-            # subject
-            "sub": user
-        }
-        return jwt.encode(payload, self.secret_key, algorithm="HS256")
-
-    def decode_token(self, token):
-        try:
-            payload = jwt.decode(token, self.secret_key, algorithms="HS256")
-            return payload["sub"]
-        except jwt.ExpiredSignatureError:
-            raise HTTPException(status_code=401, detail="Signature expired")
-        except jwt.InvalidTokenError:
-            raise HTTPException(status_code=401, detail="Invalid token")
-
-    def auth_wrapper(self, token: str = Depends(oauth2)):
-        return self.decode_token(token)
+def get_current_active_user(current_user: AuthModel = Depends(get_current_user)):
+    return current_user
