df3ad7563cfdd857ff5871b8e24a20d737a14717
Marc Müller <marc.mueller04@gmail.com>
2022-12-05 09:21:17 +0100

    Update main.py

--- docker/app/main.py
+++ docker/app/main.py
@@ -17,9 +17,9 @@ profileC = db["profile"]
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
